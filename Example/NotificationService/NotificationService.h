d02931609fac36b313a8e1a06b10029c700e49d8
Peter Wilkniss <peter@iMac.local>
2017-07-04 11:15:07 -0700

    add notification service

---
+++ Example/NotificationService/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  NotificationService
+//
+//  Created by pwilkniss on 3/20/17.
+//  Copyright © 2017 CleverTap. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
