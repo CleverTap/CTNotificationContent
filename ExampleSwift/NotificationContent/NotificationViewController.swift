f8c5b9259beba9b1cc2f3b635c89034ba4802145
Aditi3 <aditi.gvc@gmail.com>
2020-07-03 12:33:40 +0530

    task: update starter application to implement notification clicked for carousel action buttons

--- ExampleSwift/NotificationContent/NotificationViewController.swift
+++ ExampleSwift/NotificationContent/NotificationViewController.swift
@@ -1,15 +1,25 @@
 import CTNotificationContent
+import CleverTapSDK
 
 class NotificationViewController: CTNotificationViewController {
-
+    
     override func viewDidLoad() {
         self.contentType = .contentSlider // default is .contentSlider, just here for illustration
         super.viewDidLoad()
     }
     
-    // optional:  implement to get user event data
+    // optional: implement to get user event data
     override func userDidPerformAction(_ action: String, withProperties properties: [AnyHashable : Any]!) {
         print("userDidPerformAction \(action) with props \(String(describing: properties))")
     }
     
+    // optional: implement to notification payload
+    override func userDidReceive(_ response: UNNotificationResponse?) {
+        print("Push Notification Payload \(String(describing: response?.notification.request.content.userInfo))")
+        let notificationPayload = response?.notification.request.content.userInfo
+        if (response?.actionIdentifier == "action_2") {
+            CleverTap.sharedInstance()?.recordNotificationClickedEvent(withData: notificationPayload ?? "")
+            CleverTap.setDebugLevel(3)
+        }
+    }
 }
