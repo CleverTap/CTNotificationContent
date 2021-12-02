0df0a3ec6996b7652f6141a8987e88805d6cef75
Aditi3 <Aditi3@users.noreply.github.com>
2021-12-02 08:35:21 +0530

    Update to ExampleSwiftPM

--- ExampleSwiftPM/NotificationContent/NotificationViewController.swift
+++ ExampleSwiftPM/NotificationContent/NotificationViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import CTNotificationContent
-import CleverTapSDK
 
 class NotificationViewController: CTNotificationViewController {
     
@@ -19,13 +18,5 @@ class NotificationViewController: CTNotificationViewController {
     override func userDidPerformAction(_ action: String, withProperties properties: [AnyHashable : Any]!) {
         print("userDidPerformAction \(action) with props \(String(describing: properties))")
     }
-    
-    // optional: implement to get notification response
-    override func userDidReceive(_ response: UNNotificationResponse?) {
-        print("Push Notification Payload \(String(describing: response?.notification.request.content.userInfo))")
-        let notificationPayload = response?.notification.request.content.userInfo
-        if (response?.actionIdentifier == "action_2") {
-            CleverTap.sharedInstance()?.recordNotificationClickedEvent(withData: notificationPayload ?? "")
-        }
-    }
+  
 }
