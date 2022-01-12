356e7e8cc862de5968ab177f96cfc6c8c5e5f04b
kushagraa14 <kushagra@clevertap.com>
2022-01-12 13:02:31 +0530

    code reindented

--- CTNotificationContent/CTNotificationViewController.swift
+++ CTNotificationContent/CTNotificationViewController.swift
@@ -9,35 +9,36 @@ open class CTNotificationViewController: UIViewController, UNNotificationContent
     open override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
     private func setup(msg:String, data:String) {
- 
         
-                    if msg == "content-slider"{
-                        let secondVC = CarouselTemplate()
-                        secondVC.myData = data
-                        addChild(secondVC)
-                        self.view.addSubview(secondVC.view)
-                        secondVC.view.frame = self.view.bounds
+        
+        if msg == "content-slider"{
+            let secondVC = CarouselTemplate()
+            secondVC.myData = data
+            addChild(secondVC)
+            self.view.addSubview(secondVC.view)
+            secondVC.view.frame = self.view.bounds
         }
-                    if msg == "basic-template"{
-                        let secondVC = CTCaptionImageView()
-                        secondVC.myData = data
-                        addChild(secondVC)
-                        self.view.addSubview(secondVC.view)
-                        secondVC.view.frame = self.view.bounds
+        if msg == "basic-template"{
+            let secondVC = CTCaptionImageView()
+            secondVC.myData = data
+            addChild(secondVC)
+            self.view.addSubview(secondVC.view)
+            secondVC.view.frame = self.view.bounds
         }
-//                    if msg == "timer-template"{
-//
-////                        timerViewController().filldata(data: data)
-//                        let secondVC = timerViewController()
-//                        secondVC.myName = data
-//                        addChild(secondVC)
-//                        self.view.addSubview(secondVC.view)
-//                        secondVC.view.frame = self.view.bounds
-//        }
-                    
-
-        }
+        //                    if msg == "timer-template"{
+        //
+        ////                        timerViewController().filldata(data: data)
+        //                        let secondVC = timerViewController()
+        //                        secondVC.myName = data
+        //                        addChild(secondVC)
+        //                        self.view.addSubview(secondVC.view)
+        //                        secondVC.view.frame = self.view.bounds
+        //        }
+        
+        
+    }
     public func didReceive(_ notification: UNNotification) {
         let content = notification.request.content.userInfo as? [String:Any]
         setup(msg: content?["ct_template"] as! String, data: content?["ct_data"] as! String)
